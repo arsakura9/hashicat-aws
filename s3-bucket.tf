f97d4fbfc256c73a6136b15e776d42132baa618c
Srichan Sae-io <arsakura9@gmail.com>
2022-01-20 05:52:33 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
+module "s3-bucket" {
+  source  = "app.terraform.io/kyd-srichan/s3-bucket/aws"
+  version = "2.13.0"
+  # insert required variables here
   bucket_prefix = "srichan-sae-io"
-
-  versioning = {
-    enabled = true
-  }
-
 }
